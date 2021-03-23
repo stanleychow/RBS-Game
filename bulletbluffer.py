d15b56c87470d0649c2c5e54b3868101af28c28d
stanleychow <chowtech1022@gmail.com>
2021-03-23 15:28:07 -0400

    Edge cases updated
    Programmed logic for edge cases for bullet maxes and life counts

--- bulletbluffer.py
+++ bulletbluffer.py
@@ -21,7 +21,7 @@ pygame.display.set_caption('Welcome to Bullet Bluffer!')
 # Set up the drawing window
 screen = pygame.display.set_mode([600, 600])
 
-#Run until the user asks to quit
+# #Run until the user asks to quit
 running = True
 while running:
 
@@ -48,13 +48,62 @@ while running:
     text_img = font.render("R : Reload | B : Block | S : Shoot", True, GREEN)
     screen.blit(text_img, (100, 500))
 
+    #Winning/losing and bullet logic
+    if user_ammo > 0:
+        pygame.draw.circle(screen, GREEN, (150, 400), 30, 0)
+    if user_ammo > 1:
+        pygame.draw.circle(screen, GREEN, (300, 400), 30, 0)
+    if user_ammo > 2 and user_life == 1:
+        pygame.draw.circle(screen, GREEN, (450, 400), 30, 0)
+        font = pygame.font.SysFont(None, 40)
+        img = font.render('You Won!', True, GREEN)
+        screen.blit(img, (50, 50))
+    if user_ammo > 2 and user_life == 0:
+        pygame.draw.circle(screen, GREEN, (450, 400), 30, 0)
+        font = pygame.font.SysFont(None, 40)
+        img = font.render('Tie Game!', True, GREEN)
+        screen.blit(img, (50, 50))
+
+    if computer_ammo > 0:
+        pygame.draw.circle(screen, RED, (150, 200), 30, 0)
+    if computer_ammo > 1:
+        pygame.draw.circle(screen, RED, (300, 200), 30, 0)
+    if computer_ammo > 2 and computer_life == 1:
+        pygame.draw.circle(screen, RED, (450, 200), 30, 0)
+        font = pygame.font.SysFont(None, 40)
+        img = font.render('You Lost!', True, RED)
+        screen.blit(img, (50, 50))
+    if user_ammo > 2 and user_life == 0:
+        pygame.draw.circle(screen, GREEN, (450, 400), 30, 0)
+        font = pygame.font.SysFont(None, 40)
+        img = font.render('Tie Game!', True, GREEN)
+        screen.blit(img, (50, 50))
+
+    if computer_life == 0 and user_life > 0:
+        font = pygame.font.SysFont(None, 40)
+        img = font.render('You Won!', True, GREEN)
+        screen.blit(img, (50, 50))
+    if user_life == 0 and computer_life > 0:
+        font = pygame.font.SysFont(None, 40)
+        img = font.render('You Lost!', True, RED)
+        screen.blit(img, (50, 50))
+    if computer_life == 0 and user_life == 0:
+        font = pygame.font.SysFont(None, 40)
+        img = font.render('Tie Game!', True, RED)
+        screen.blit(img, (50, 50))
+
+
+
+
+
+
     # Update the game display
     pygame.display.update()
 
     keys = pygame.key.get_pressed()
     computer_rbs = ['r', 'b', 's']
     # Update the player
-    # If user presses R to Reload
+    #If user presses R to Reload
     if keys[pygame.K_r]:
         enemy_move = random.choice(computer_rbs)
         #And computer Reloads
@@ -67,53 +116,53 @@ while running:
         #And computer Shoots
         if enemy_move == "s":
             if computer_ammo > 0:
-            user_life -= 1
-            computer_ammo -= 1
+                user_life -= 1
+                computer_ammo -= 1
             if computer_ammo <= 0:
                 user_ammo += 1
 
-    #If user presses B to Block
-    if keys[pygame.K_b]:
-        enemy_move = random.choice(computer_rbs)
-        #And computer Reloads
-        if enemy_move == "r":
-            computer_ammo += 1
-        #And computer Blocks
-        if enemy_move == "b":
-            pass
-        #And computer Shoots
-        if enemy_move == "s":
-            computer_ammo -=1
+#     #If user presses B to Block
+#     if keys[pygame.K_b]:
+#         enemy_move = random.choice(computer_rbs)
+#         #And computer Reloads
+#         if enemy_move == "r":
+#             computer_ammo += 1
+#         #And computer Blocks
+#         if enemy_move == "b":
+#             pass
+#         #And computer Shoots
+#         if enemy_move == "s":
+#             computer_ammo -=1
 
-    #If user presses S to Shoot   
-    if keys[pygame.K_s]:
-        enemy_move = random.choice(computer_rbs)
-        #And computer Reloads
-        if enemy_move == "r":
-            if user_ammo > 0:
-                user_ammo -= 1
-                computer_life -= 1
-            if user_ammo <= 0:
-                computer_ammo += 1
-        #And computer Blocks
-        if enemy_move == "b":
-            if user_ammo > 0:
-                user_ammo -=1
-            if user_ammo <= 0:
-                pass
-        #And computer Shoots
-        if enemy_move == "s":
-            if user_ammo > 0:
-                if computer_ammo > 0:
-                    user_ammo -= 1
-                    computer_ammo -= 1
-                if computer_ammo <=0:
-                    user_ammo -=1
-                    computer_life -= 1
-            if user_ammo <= 0:
-                if computer_ammo > 0:
-                    computer_ammo -= 1
-                if computer_ammo <=0:
+#     #If user presses S to Shoot   
+#     if keys[pygame.K_s]:
+#         enemy_move = random.choice(computer_rbs)
+#         #And computer Reloads
+#         if enemy_move == "r":
+#             if user_ammo > 0:
+#                 user_ammo -= 1
+#                 computer_life -= 1
+#             if user_ammo <= 0:
+#                 computer_ammo += 1
+#         #And computer Blocks
+#         if enemy_move == "b":
+#             if user_ammo > 0:
+#                 user_ammo -=1
+#             if user_ammo <= 0:
+#                 pass
+#         #And computer Shoots
+#         if enemy_move == "s":
+#             if user_ammo > 0:
+#                 if computer_ammo > 0:
+#                     user_ammo -= 1
+#                     computer_ammo -= 1
+#                 if computer_ammo <=0:
+#                     user_ammo -=1
+#                     computer_life -= 1
+#             if user_ammo <= 0:
+#                 if computer_ammo > 0:
+#                     computer_ammo -= 1
+#                 if computer_ammo <=0:
 
         
     
